7c44577d3a2f71f05568c267a0c9356663f6915b
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-21 12:36:14 -0400

    Kyle Thousand

--- sharingElements.js
+++ sharingElements.js
@@ -1,11 +1,15 @@
 //SHARING ELEMENTS
 
 const sharingElements = (arr, size) => {
+  if (size != arr.length) {
+    let arr2 = arr.splice(size);
+    return [arr, arr2];
+  }
+  return [arr];
+};
 
-}
-
-console.log(sharingElements(["a", "b", "c", "d"], 3))
+console.log(sharingElements(["a", "b", "c", "d"], 3));
 
 module.exports = {
-  sharingElements
-}
+  sharingElements,
+};
